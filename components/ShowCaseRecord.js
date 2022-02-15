16a9379366b027f34d467cb85e163c236ad86378
mrcanterbury <mattrcanterbury@gmail.com>
2022-02-15 13:02:07 -0600

    added embla carousel

--- components/ShowCaseRecord.js
+++ components/ShowCaseRecord.js
@@ -1,14 +1,21 @@
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
 import Image from 'next/image'
 import showcase from '../src/styles/modules/Showcase.module.scss'
 
 export default function ShowCaseRecord({details}) {
   console.log(details.showCaseGallery)
+  const [emblaRef] = useEmblaCarousel()
   return (
     <>
       <div className={showcase.gallery}>
-      {details.showCaseGallery.map(card => <div key={card.id}>
-        <Image src={card.url} width={card.width} height={card.height} priority />
-      </div>)}
+        <div className={showcase.embla} ref={emblaRef}>
+          <div className={showcase.embla__container}>
+            {details.showCaseGallery.map(card => <div className={showcase.embla__slide} key={card.id}>
+              <Image src={card.url} width={card.width} height={card.height} priority />
+            </div>)}
+          </div>
+        </div>
       </div>
     </>
   )
